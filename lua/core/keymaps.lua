abfebcf869c4b8185ce34d8aaab23997d7bb897a
MohAlkurdi <imohkurdi@gmail.com>
2023-07-08 02:24:47 +0300

    add: J and K keymaps
    Added J and K in visual mode to easily move lines up and down

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,6 +12,10 @@ keymap.set("i", "jk", "<ESC>")
 
 -- use <leader> w for fast save
 keymap.set("n", "<leader>w", ":write<CR>", { desc = "Save" })
+
+-- use J & K to move lines up and down
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 --------------------
 -- Plugin Keybinds
 ----------------------
