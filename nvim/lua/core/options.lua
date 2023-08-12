ce0c9b78961e0b2cb3753c992501d1acabe49e63
MohAlkurdi <imohkurdi@gmail.com>
2023-08-12 21:49:47 +0300

    add: highlight on yank

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -42,3 +42,11 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd([[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+augroup END
+]])
