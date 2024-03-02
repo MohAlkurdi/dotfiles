9a276934ba0b33b4599bf5a5c9fbf804c746a068
MohAlkurdi <imohkurdi@gmail.com>
2024-03-02 21:00:54 +0300

    add support for tailwind classes in .erb file

--- nvim/lua/moh/plugins/colorscheme.lua
+++ nvim/lua/moh/plugins/colorscheme.lua
@@ -10,26 +10,26 @@
 -- }
 
 -- Sonokai Theme
-return {
-	"sainnhe/sonokai",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.g.sonokai_style = "atlantis" -- atlantis - andromeda - shusia - maia - espresso
-		vim.g.sonokai_enable_italic = 1
-		vim.g.sonokai_transparent_background = 1
-		vim.cmd([[colorscheme sonokai]])
-	end,
-	opts = {},
-}
+-- return {
+-- 	"sainnhe/sonokai",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.g.sonokai_style = "atlantis" -- atlantis - andromeda - shusia - maia - espresso
+-- 		vim.g.sonokai_enable_italic = 1
+-- 		vim.g.sonokai_transparent_background = 1
+-- 		vim.cmd([[colorscheme sonokai]])
+-- 	end,
+-- 	opts = {},
+-- }
 
 -- catppuccin Theme
--- return {
--- 	"catppuccin/nvim",
--- 	name = "catppuccin",
--- 	priority = 1000,
--- 	lazy = false,
--- 	config = function()
--- 		vim.cmd([[colorscheme catppuccin-frappe]]) -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
--- 	end,
--- }
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd([[colorscheme catppuccin-frappe]]) -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+	end,
+}
