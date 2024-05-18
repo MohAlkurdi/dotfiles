0bcfd8acd4f8e1f295d2e1fe82619dd3a92d6335
MohAlkurdi <imohkurdi@gmail.com>
2024-05-18 12:48:41 +0300

    updates some lsps

--- nvim/lua/moh/plugins/colorscheme.lua
+++ nvim/lua/moh/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 -- Tokyonight Theme
-return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd([[colorscheme tokyonight]]) -- storm - moon - night - day.
-	end,
-	opts = {},
-}
+-- return {
+-- 	"folke/tokyonight.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd([[colorscheme tokyonight]]) -- storm - moon - night - day.
+-- 	end,
+-- 	opts = {},
+-- }
 
 -- Sonokai Theme
 -- return {
@@ -24,33 +24,12 @@ return {
 -- }
 
 -- catppuccin Theme
--- return {
--- 	"catppuccin/nvim",
--- 	name = "catppuccin",
--- 	priority = 1000,
--- 	lazy = false,
--- 	config = function()
--- 		vim.cmd([[colorscheme catppuccin-frappe]]) -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
--- 	end,
--- }
-
--- Lua
-
--- poimandres Theme
--- return {
--- 	"olivercederborg/poimandres.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("poimandres").setup({
--- 			-- leave this setup function empty for default config
--- 			-- or refer to the configuration section
--- 			-- for configuration options
--- 		})
--- 	end,
---
--- 	-- optionally set the colorscheme within lazy config
--- 	init = function()
--- 		vim.cmd("colorscheme poimandres")
--- 	end,
--- }
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd([[colorscheme catppuccin-macchiato]]) -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+	end,
+}
