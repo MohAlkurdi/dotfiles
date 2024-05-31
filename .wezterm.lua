c6b53c34d0b86627e4c0706907b9d8f5eb1e28a4
MohAlkurdi <imohkurdi.com>
2024-05-31 06:12:52 +0300

    add support for rtl text && more fonts option

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Window config
-config.window_background_opacity = 0.95
 config.window_padding = {
 	left = 0,
 	right = 0,
@@ -14,8 +13,15 @@ config.window_padding = {
 }
 
 -- Font
-config.font = wezterm.font("BlexMono Nerd Font")
-config.font_size = 16
+config.font_size = 17
+-- config.font = wezterm.font("BlexMono Nerd Font") -- 16
+-- config.font = wezterm.font("JetBrainsMono Nerd Font") -- 16
+-- config.font = wezterm.font("CaskaydiaCove Nerd Font") -- 16
+config.font = wezterm.font("ComicShannsMono Nerd Font") -- 17
+config.bidi_enabled = true
+
+-- disable bell sound
+config.audible_bell = "Disabled"
 
 -- and finally, return the configuration to wezterm
 return config
