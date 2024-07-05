9e7878ad78a147509787554bbd63b4c4b9af76b1
MohAlkurdi <imohkurdi@gmail.com>
2024-07-05 22:00:50 +0300

    add more themes

--- nvim/lua/moh/plugins/colorscheme.lua
+++ nvim/lua/moh/plugins/colorscheme.lua
@@ -1,42 +1,83 @@
--- Tokyonight Theme
--- return {
--- 		"folke/tokyonight.nvim",
--- 		lazy = false,
--- 		priority = 1000,
--- 		config = function()
--- 			vim.cmd([[colorscheme tokyonight]]) -- storm - moon - night - day.
--- 		end,
--- 		opts = {},
--- 	}
-
--- catppuccin Theme
--- return {
--- 		"catppuccin/nvim",
--- 		name = "catppuccin",
--- 		priority = 1000,
--- 		lazy = false,
--- 		config = function()
--- 			vim.cmd([[colorscheme catppuccin-frappe]]) -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
--- 		end,
--- 	}
-
--- Night owl
--- return {
--- 		"oxfist/night-owl.nvim",
--- 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
--- 		priority = 1000, -- make sure to load this before all the other start plugins
--- 		config = function()
--- 			-- load the colorscheme here
--- 			require("night-owl").setup()
--- 			vim.cmd.colorscheme("night-owl")
--- 		end,
--- 	}
-
--- Palenight Theme
 return {
-	"alexmozaidze/palenight.nvim",
-	priority = 1000,
-	config = function()
-		vim.cmd([[colorscheme palenight]])
-	end,
+	-- Tokyonight Theme
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme tokyonight]]) -- storm - moon - night - day.
+		end,
+		opts = {},
+	},
+
+	-- catppuccin Theme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			vim.cmd([[colorscheme catppuccin-frappe]]) -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+		end,
+	},
+
+	-- Night owl
+	{
+		"oxfist/night-owl.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			require("night-owl").setup()
+			vim.cmd.colorscheme("night-owl")
+		end,
+	},
+
+	-- Palenight Theme
+	{
+		"alexmozaidze/palenight.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme palenight]])
+		end,
+	},
+
+	-- Poimandres Theme
+	{
+		"olivercederborg/poimandres.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("poimandres").setup({})
+			vim.cmd([[colorscheme poimandres]])
+		end,
+	},
+
+	-- Onedark Theme
+	{
+		"navarasu/onedark.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme onedark]])
+		end,
+	},
+
+	-- Onenord theme
+	{
+		"rmehri01/onenord.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme onenord]])
+		end,
+	},
+
+	-- Solarized Osaka Theme
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
 }
