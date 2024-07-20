c37cd0ec2e853d89a934767876943eeb1dd2175e
MohAlkurdi <imohkurdi@gmail.com>
2024-07-20 04:38:32 +0300

    a lot of changes
    - removed trouble.lua since i'm not using that plugin
    - migrate to the new which-key mapping
    - add luffy and brook to my alpha-nvim plugin (:

--- nvim/lua/moh/plugins/which-key.lua
+++ nvim/lua/moh/plugins/which-key.lua
@@ -2,66 +2,57 @@ return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
 	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 500
-
 		local wk = require("which-key")
-		wk.register({
-			["<leader>"] = {
-				f = {
-					name = "+Find",
-					f = { "<cmd>Telescope find_files<cr>", "Find File" },
-					r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-					n = { "<cmd>enew<cr>", "New File" },
-					t = { ":TodoTelescope<CR>", "Find Todo" },
-					s = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-					-- c = { "<cmd>Telescope grep_string<cr>", "Find String Under Cursor" },
-					b = { "<cmd>Telescope buffers<cr>", "Find Buffer" },
-					g = { ":LazyGit<CR>", "LazyGit" },
-					h = { "<cmd>Telescope help_tags<cr>", "Find help" },
-					c = { ":Telescope colorscheme<CR>", "Colorscheme" },
+		wk.add({
+			{
+				-- Buffer
+				{ "<leader>b", group = "Buffer" },
+				{ "<leader>ba", ":%bd<CR>", desc = "Close All buffer" },
+				{ "<leader>bc", ":BufferLinePickClose<CR>", desc = "Close" },
+				{ "<leader>bd", "<cmd>bdelete<CR>", desc = "Delete" },
+
+				-- Code Action
+				{ "<leader>c", group = "Code Action" },
+				{ "<leader>cD", "<cmd>Telescope diagnostics bufnr=0<CR>", desc = "Show buffer diagnostics" },
+				{ "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", desc = "Available Code Action" },
+				{ "<leader>cd", "<Cmd>lua vim.diagnostic.open_float()<CR>", desc = "Show Line Diagnostics" },
+				{ "<leader>co", "<cmd>:%bd|e#|bd#<CR>", desc = "Close other tabs" },
+				{ "<leader>cr", "<Cmd>lua vim.lsp.buf.rename()<CR>", desc = "Smart rename" },
+				{ "<leader>cs", ":LspRestart<CR>", desc = "Restart LSP" },
+				{
+					"<leader>cw",
+					":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+					desc = "Change Word under cursor",
 				},
-				b = {
-					name = "+Buffer",
-					d = { "<cmd>bdelete<CR>", "Delete" },
-					c = { ":BufferLinePickClose<CR>", "Close" },
-					a = { ":%bd<CR>", "Close All buffer" },
-				},
-				x = {
-					name = "+Trouble",
-					x = { "<cmd>TroubleToggle<cr>", "Troble Toggle" },
-					w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace Diagnostics" },
-					d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Document Diagnostics" },
-					l = { "<cmd>TroubleToggle loclist<cr>", "Loc List" },
-					q = { "<cmd>TroubleToggle quickfix<cr>", "Quick Fix" },
-				},
-				g = {
-					name = "+Go To",
-					R = { "<cmd>Telescope lsp_references<CR>", "Show LSP references" },
-					D = { "<Cmd>lua vim.lsp.buf.declaration()<CR>", "Go to declaration" },
-					d = { "<cmd>Telescope lsp_definitions<CR>", "Show lSP definitions" },
-					i = { "<cmd>Telescope lsp_implementations<CR>", "Go to implementation" },
-					t = { "<cmd>Telescope lsp_type_definitions<CR>", "Show LSP type definitions" },
-					p = { "<cmd>Gitsigns preview_hunk<CR>", "Show Git Hunk" },
-				},
-				c = {
-					name = "+Code Action",
-					a = { "<Cmd>lua vim.lsp.buf.code_action()<CR>", "Available Code Action" },
-					r = { "<Cmd>lua vim.lsp.buf.rename()<CR>", "Smart rename" },
-					d = { "<Cmd>lua vim.diagnostic.open_float()<CR>", "Show Line Diagnostics" },
-					D = { "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics" },
-					s = { ":LspRestart<CR>", "Restart LSP" },
-					w = { ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", "Change Word under cursor" },
-					o = { "<cmd>:%bd|e#|bd#<CR>", "Close other tabs" },
-				},
-				s = {
-					name = "+Window Split",
-					v = { "<C-w>v", "Split Vertically" },
-					h = { "<C-w>s", "Split horizontally" },
-					e = { "<C-w>=", "Resize Split" },
-					x = { ":close<CR>", "Close Split" },
-					s = { ":setlocal spell!<CR>", "Set Spell" },
-				},
+
+				-- Find
+				{ "<leader>f", group = "Find" },
+				{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffer" },
+				{ "<leader>fc", ":Telescope colorscheme<CR>", desc = "Colorscheme" },
+				{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
+				{ "<leader>fg", ":LazyGit<CR>", desc = "LazyGit" },
+				{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find help" },
+				{ "<leader>fn", "<cmd>enew<cr>", desc = "New File" },
+				{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File" },
+				{ "<leader>fs", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+				{ "<leader>ft", ":TodoTelescope<CR>", desc = "Find Todo" },
+
+				-- Go To
+				{ "<leader>g", group = "Go To" },
+				{ "<leader>gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", desc = "Go to declaration" },
+				{ "<leader>gR", "<cmd>Telescope lsp_references<CR>", desc = "Show LSP references" },
+				{ "<leader>gd", "<cmd>Telescope lsp_definitions<CR>", desc = "Show lSP definitions" },
+				{ "<leader>gi", "<cmd>Telescope lsp_implementations<CR>", desc = "Go to implementation" },
+				{ "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", desc = "Show Git Hunk" },
+				{ "<leader>gt", "<cmd>Telescope lsp_type_definitions<CR>", desc = "Show LSP type definitions" },
+
+				-- Window Split
+				{ "<leader>s", group = "Window Split" },
+				{ "<leader>se", "<C-w>=", desc = "Resize Split" },
+				{ "<leader>sh", "<C-w>s", desc = "Split horizontally" },
+				{ "<leader>ss", ":setlocal spell!<CR>", desc = "Set Spell" },
+				{ "<leader>sv", "<C-w>v", desc = "Split Vertically" },
+				{ "<leader>sx", ":close<CR>", desc = "Close Split" },
 			},
 		})
 	end,
