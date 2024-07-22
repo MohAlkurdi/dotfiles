64b41c123c810b38c30ff18af057136299d1072f
MohAlkurdi <imohkurdi@gmail.com>
2024-07-22 06:35:11 +0300

    add transparent colorscheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,11 +13,13 @@ config.window_padding = {
 }
 
 -- Font
+-- config.line_height = 1.2
 config.font_size = 18
 -- config.font = wezterm.font("BlexMono Nerd Font") -- 17
 -- config.font = wezterm.font("JetBrainsMono Nerd Font") -- 17
 -- config.font = wezterm.font("CaskaydiaCove Nerd Font") -- 17
 config.font = wezterm.font("ComicShannsMono Nerd Font") -- 18
+-- config.font = wezterm.font("MesloLGL Nerd Font") -- 17
 config.bidi_enabled = true
 
 -- disable bell sound
@@ -25,6 +27,13 @@ config.audible_bell = "Disabled"
 
 -- disable the title bar but enable the resizable border
 config.window_decorations = "RESIZE"
+
+-- set window opacity and transparency
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+-- set the tab bar to be at the bottom
+config.tab_bar_at_bottom = true
 
 -- and finally, return the configuration to wezterm
 return config
