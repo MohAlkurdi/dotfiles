0a11f81e229f67a7f0f978bb65a6fce80faa7d72
MohAlkurdi <imohkurdi@gmail.com>
2024-07-22 06:35:11 +0300

    add transparent colorscheme

--- nvim/lua/moh/plugins/colorscheme.lua
+++ nvim/lua/moh/plugins/colorscheme.lua
@@ -6,31 +6,17 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd([[colorscheme tokyonight]]) -- storm - moon - night - day.
+
+			local transparent = true -- set to true if you would like to enable transparency
+			require("tokyonight").setup({
+				transparent = transparent,
+				styles = {
+					sidebars = transparent and "transparent" or "dark",
+					floats = transparent and "transparent" or "dark",
+				},
+			})
 		end,
 		opts = {},
-	},
-
-	-- catppuccin Theme
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		lazy = false,
-		config = function()
-			vim.cmd([[colorscheme catppuccin-frappe]]) -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-		end,
-	},
-
-	-- Night owl
-	{
-		"oxfist/night-owl.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			require("night-owl").setup()
-			vim.cmd.colorscheme("night-owl")
-		end,
 	},
 
 	-- Palenight Theme
@@ -49,16 +35,15 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd([[colorscheme onedark]])
-		end,
-	},
 
-	-- Onenord theme
-	{
-		"rmehri01/onenord.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme onenord]])
+			local transparent = true -- set to true if you would like to enable transparency
+			require("onedark").setup({
+				transparent = transparent,
+				styles = {
+					sidebars = transparent and "transparent" or "dark",
+					floats = transparent and "transparent" or "dark",
+				},
+			})
 		end,
 	},
 
@@ -81,7 +66,8 @@ return {
 		-- Optional; default configuration will be used if setup isn't called.
 		config = function()
 			require("everforest").setup({
-				-- Your config here
+
+				transparent_background_level = 5,
 			})
 		end,
 	},
